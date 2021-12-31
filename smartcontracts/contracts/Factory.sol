8203b1a90bd4bc4560f73b507aca3fe9a093c8c7
ahmetson <ahmetson@zoho.com>
2021-12-31 12:01:39 +0700

    Keep factory simplified

--- smartcontracts/contracts/Factory.sol
+++ smartcontracts/contracts/Factory.sol
@@ -2,7 +2,7 @@ pragma solidity =0.5.16;
 
 import './interfaces/FactoryInterface.sol';
 import './interfaces/IERC20.sol';
-import './CrosschainHalfPair.sol';
+import './Pair.sol';
 
 contract Factory is FactoryInterface {
     address public feeTo;
@@ -10,58 +10,38 @@ contract Factory is FactoryInterface {
 
     address public verifierManager;
 
-    // first chain id => last chain id => token 0 => token 1 => pair
-    mapping(uint256 => mapping(uint256 => mapping(address => mapping(address => address)))) public getPair;
+    // token on this blockchain => token on the target blockchain => pair
+    mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
-    // The pair of the Blockchains.
-    // The first blockchain is the blockchain is the blockchain
-    // where the event has to be initiated.
-    mapping(uint256 => uint256) public firstToLastCrosses;
+    uint targetChainID;
 
-    event ChanPairCreated(uint256 _firstChainID, uint256 _lastChainID, uint256 _lastOffset);
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint pairNumber, uint chain0, uint chain1);
+    event ChanPairCreated(uint _firstChainID, uint _targetChainID);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint pairNumber);
 
     // Round offset of this blockchain.
     // another blockchain's id => offset of this blockchain against another.
-    mapping(uint256 => uint256) public offsets;
+    mapping(uint => uint) public offsets;
 
     /**
-     *  @notice Deploy this Factory on one of the blockchains.
-     *  When deploying, define the blockchain pairs.
-     *  
-     *  @param _firstChainID    -   first part of a blockchain pair.
-     *                              user interacts with crosschain liquidity
-     *                              in this blockchain first.
-     *
-     *  @param _lastChainID   -     the last part of a blockchain pair.
-     *  @param _lastOffset    -     if the blockchain where this contract
-     *                              is deployed, then set the offset.
+     *  @notice Factory is deployed per blockchain pair.
+     *  @param _targetChainID     the paired blockchain to the blockchain where this Smartcontract was deployed.
      */
-    constructor(address payable _feeToSetter, address _verifierManager, uint256 _firstChainID, uint256 _lastChainID, uint256 _lastOffset) public {
-        require(addBlockchainPair(_firstChainID, _lastChainID, _lastOffset), "CHAIN_PAIR_FAILED");
-        feeToSetter = _feeToSetter;
-        verifierManager = _verifierManager;
-    }
-
-    /// todo add verifier that another blockchain also created a pair.
-    function addBlockchainPair(uint256 _firstChainID, uint256 _lastChainID, uint256 _lastOffset) public returns (bool) {
+    constructor(
+        address payable _feeToSetter, 
+        address         _verifierManager, 
+        uint            _targetChainID
+    ) public {
         require(feeToSetter == address(0) || feeToSetter == msg.sender, "FORBIDDEN");
-
-        uint256 _chainID = getChainID();
-        require(_chainID == _firstChainID || _chainID == _lastChainID, "XDEX: INVALID_CHAIN_ID");
         
-        if (_firstChainID == _chainID) {
-            firstToLastCrosses[_firstChainID]   = _lastChainID;
-            offsets[_firstChainID]              = 0;
-        } else {
-            firstToLastCrosses[_lastChainID]    = _firstChainID;
-            offsets[_lastChainID]               = _lastOffset;
-        }
+        uint thisChainID = getChainID();
+        require(_targetChainID > 0 && _targetChainID != thisChainID, "Invalid target blockchain");
 
-        emit ChanPairCreated(_firstChainID, _lastChainID, _lastOffset);
+        feeToSetter     = _feeToSetter;
+        verifierManager = _verifierManager;
+        targetChainID   = _targetChainID;
 
-        return true;
+        emit ChanPairCreated(thisChainID, targetChainID);
     }
 
     function allPairsLength() external view returns (uint) {
@@ -72,8 +52,9 @@ contract Factory is FactoryInterface {
      *  on this Blockchain. While another token is on another Blockchain.
      * 
      *  @param tokens - first token is the token on this blockchain.
-     *  and second token is the token on another blockchain.
-     *  @param targetChainID - is the blockchain where second token at.
+     *  and second token is the token on the target blockchain.
+     *  @param amounts - amount of tokens to put on this blockchain.
+     *  and second element is the amount on the target blockchain.
      *  
      *  @dev User submits the token on the first blockchain.
      *  then on the target blockchain. 
@@ -85,47 +66,32 @@ contract Factory is FactoryInterface {
      */
     function createPair(
         address[2] calldata tokens, // token 0, token 1
-        uint256[2] calldata amounts, // amount 0, amount 1
-        uint256 targetChainID
+        uint[2] calldata amounts // amount 0, amount 1
     ) external returns (address pair) {
-        uint256 thisChainID = getChainID();
-        require(targetChainID != thisChainID && targetChainID != 0, 'INVALID_CHAIN_ID');
-        require (amounts[0] > 0 && amounts[1] > 0, 'ZERO_AMOUNT');
-        
-        require(tokens[0] != address(0) && tokens[1] != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(validBlockchainPair(thisChainID, targetChainID), 'INVALID_BLOCKCHAIN_PAIR');
+        uint thisChainID = getChainID();
+        require(amounts[0] > 0 && amounts[1] > 0, 'ZERO_AMOUNT');
+        require(tokens[0] != address(0) && tokens[1] != address(0), 'ZERO_ADDRESS');
 
-        (uint256 chain0, uint256 chain1) = chainPairOrder(thisChainID, targetChainID);
-        
-        require(getPair[chain0][chain1][tokens[0]][tokens[1]] == address(0), 'PAIR_EXISTS');
+        require(getPair[tokens[0]][tokens[1]] == address(0), 'PAIR_EXISTS');
 
-        bytes memory bytecode = type(CrosschainHalfPair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(chain0, chain1, tokens[0], tokens[1]));
+        // Creating the Pair contract
+        bytes memory bytecode = type(Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(thisChainID, targetChainID, tokens[0], tokens[1]));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
 
-        bool firstChain = firstChain(thisChainID);
+        require(IERC20(tokens[0]).transferFrom(msg.sender, pair, amounts[0]), "FAILED_TO_TRANSFER_TOKEN");
 
-        if (firstChain) {
-            require(IERC20(tokens[0]).transferFrom(msg.sender, pair, amounts[0]), "FAILED_TO_TRANSFER_TOKEN");
-        } else {
-            require(IERC20(tokens[1]).transferFrom(msg.sender, pair, amounts[1]), "FAILED_TO_TRANSFER_TOKEN");
-        }
+        Pair(pair).initialize(tokens, amounts, msg.sender, verifierManager);
 
-        CrosschainHalfPair(pair).initialize(firstChain, chain0, tokens[0], 
-            chain1, tokens[1], 
-            amounts, offsets[thisChainID], 
-            [msg.sender, verifierManager]
-        );
         // populate mapping in the reverse direction
-        getPair[chain0][chain1][tokens[0]][tokens[1]] = pair;
-        getPair[chain0][chain1][tokens[1]][tokens[0]] = pair;
-        getPair[chain1][chain0][tokens[0]][tokens[1]] = pair;
-        getPair[chain1][chain0][tokens[1]][tokens[0]] = pair;
+        getPair[tokens[0]][tokens[1]] = pair;
+        getPair[tokens[1]][tokens[0]] = pair;
 
         allPairs.push(pair);
-        emit PairCreated(tokens[0], tokens[1], pair, allPairs.length, chain0, chain1);
+        
+        emit PairCreated(tokens[0], tokens[1], pair, allPairs.length);
 
         return pair;
     }
@@ -140,30 +106,11 @@ contract Factory is FactoryInterface {
         feeToSetter = _feeToSetter;
     }
 
-    function getChainID() public view returns (uint256) {
-        uint256 id;
+    function getChainID() public view returns (uint) {
+        uint id;
         assembly {
             id := chainid()
         }
         return id;
-    }
-
-    function validBlockchainPair(uint256 thisChainID, uint256 targetChainID) public view returns (bool) {
-        return firstToLastCrosses[thisChainID] == targetChainID
-        || firstToLastCrosses[targetChainID] == thisChainID;
-    }
-
-    function chainPairOrder(uint256 thisChainID, uint256 targetChainID) public view returns (uint256, uint256) {
-        if (firstToLastCrosses[thisChainID] == targetChainID) {
-            return (thisChainID, targetChainID);
-        } else if (firstToLastCrosses[targetChainID] == thisChainID) {
-            return (targetChainID, thisChainID);
-        } else {
-            return (0, 0);
-        }
-    }
-
-    function firstChain(uint256 thisChainID) public view returns (bool) {
-        return firstToLastCrosses[thisChainID] > 0;
     }
 }
